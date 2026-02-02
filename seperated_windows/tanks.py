acd987db79ecf2b43c8559c46ea8ae2e7100f43b
savand436 <sav_and@outlook.com>
2026-02-03 04:12:45 +0700

    Update tanks.py

--- seperated_windows/tanks.py
+++ seperated_windows/tanks.py
@@ -1,167 +1,229 @@
 import arcade
+import math
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Red vs Blue: Tank Battle"
 TANK_SCALE = 1
-TANK_SPEED = 3
+TANK_SPEED = 1.5
 
 ROCKET_SCALE = 0.5
-ROCKET_SPEED = 4
+ROCKET_SPEED = 5
 
 
-class Tank(arcade.Sprite):
-    def __init__(self, color, x, y):
-        super().__init__(f"images/tank_{color}.png", TANK_SCALE)
+class Tank_red(arcade.Sprite):
+    def __init__(self):
+        super(Tank_red, self).__init__()
+        self.texture = arcade.load_texture(f'Assets/images/tank_red.png')
+        self.center_x = arcade.get_window().width // 2
+        self.center_y = arcade.get_window().height // 4
 
-        self.center_x = x
-        self.center_y = y
-        self.change_x = 0
-        self.change_y = 0
-        self.color = color
-
-    def update(self):
+    def update(self, delta_time):
         self.center_x += self.change_x
         self.center_y += self.change_y
 
         if self.left < 0:
             self.left = 0
-        elif self.right > SCREEN_WIDTH:
-            self.right = SCREEN_WIDTH
+        elif self.right > arcade.get_window().width:
+            self.right = arcade.get_window().width
 
         if self.bottom < 0:
             self.bottom = 0
-        elif self.top > SCREEN_HEIGHT:
-            self.top = SCREEN_HEIGHT
+        elif self.top > arcade.get_window().height:
+            self.top = arcade.get_window().height
 
-    def shoot(self):
-        rocket = Rocket(self.center_x, self.center_y, self.color)
+        if self.change_x != 0 or self.change_y != 0:
+            angle_rad = math.atan2(self.change_x, self.change_y)
+            self.angle = math.degrees(angle_rad)
+
+    def shoot(self, angle):
+        rocket = Rocket_red(self.center_x, self.center_y, angle)
         return rocket
 
 
-class Rocket(arcade.Sprite):
-    def __init__(self, x, y, owner_color):
-        super().__init__(f"images / rocket{owner_color}.png", ROCKET_SCALE)
+class Tank_blue(arcade.Sprite):
+    def __init__(self):
+        super(Tank_blue, self).__init__()
+        self.texture = arcade.load_texture(f'Assets/images/tank_blue.png')
+        self.center_x = arcade.get_window().width // 2
+        self.center_y = arcade.get_window().height * 3 // 4
 
-        self.center_x = x
-        self.center_y = y
-        self.owner_color = owner_color
+    def update(self, delta_time):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
 
-    def update(self):
-        if self.owner_color == "red":
-            self.center_y += ROCKET_SPEED
-        else:
-            self.center_y -= ROCKET_SPEED
+        if self.left < 0:
+            self.left = 0
+        elif self.right > arcade.get_window().width:
+            self.right = arcade.get_window().width
 
-        if (self.owner_color == "red" and self.bottom > SCREEN_HEIGHT) or \
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > arcade.get_window().height:
+            self.top = arcade.get_window().height
+
+        if self.change_x != 0 or self.change_y != 0:
+            angle_rad = math.atan2(self.change_x, self.change_y)
+            self.angle = math.degrees(angle_rad)
+
+    def shoot(self, angle):
+        rocket = Rocket_blue(self.center_x, self.center_y, angle)
+        return rocket
+
+
+class Rocket_red(arcade.Sprite):
+    def __init__(self, start_x, start_y, angle):
+        super().__init__(arcade.load_texture(':resources:/images/topdown_tanks/tankRed_barrel1.png'), ROCKET_SCALE)
+        self.texture = arcade.load_texture(':resources:/images/topdown_tanks/tankRed_barrel1.png')
+        self.center_x = start_x
+        self.center_y = start_y
+        self.angle = angle
+        self.owner_color = "red"
+        self.angle = math.radians(angle - 90)
+        self.change_x = ROCKET_SPEED * math.cos(self.angle)
+        self.change_y = ROCKET_SPEED * math.sin(self.angle)
+        if self.change_x != 0 or self.change_y != 0:
+            angle_rad = math.atan2(self.change_x, self.change_y)
+            self.angle = math.degrees(angle_rad)
+
+    def update(self, delta_time):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+        if (self.owner_color == "red" and self.bottom > arcade.get_window().height) or \
+                (self.owner_color == "blue" and self.top < 0):
+            self.remove_from_sprite_lists()
+
+
+class Rocket_blue(arcade.Sprite):
+    def __init__(self, start_x, start_y, angle):
+        super().__init__(arcade.load_texture(':resources:/images/topdown_tanks/tankBlue_barrel1.png'), ROCKET_SCALE)
+        self.texture = arcade.load_texture(':resources:/images/topdown_tanks/tankBlue_barrel1.png')
+        self.center_x = start_x
+        self.center_y = start_y
+        self.angle = angle
+        self.owner_color = "blue"
+        self.angle = math.radians(angle - 90)
+        self.change_x = ROCKET_SPEED * math.cos(self.angle)
+        self.change_y = ROCKET_SPEED * math.sin(self.angle)
+        if self.change_x != 0 or self.change_y != 0:
+            angle_rad = math.atan2(self.change_x, self.change_y)
+            self.angle = math.degrees(angle_rad)
+
+    def update(self, delta_time):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+        if (self.owner_color == "red" and self.bottom > arcade.get_window().height) or \
                 (self.owner_color == "blue" and self.top < 0):
             self.remove_from_sprite_lists()
 
 
 class TankGame(arcade.Window):
-    def __init__(self, width, height, title):
-        super().__init__(width, height, title)
-        arcade.set_background_color(arcade.color.DARK_GREEN)
-
-        self.red_tank = None
-        self.blue_tank = None
-        self.rockets = None
+    def __init__(self):
+        super().__init__()
+        self.texture_back = arcade.load_texture(f'Assets/images/place_of_tanks.png')
+        self.red_tank = Tank_red()
+        self.blue_tank = Tank_blue()
         self.game_over = False
+        self.setup()
 
     def setup(self):
-
-        self.red_tank = Tank('red', SCREEN_WIDTH // 2, SCREEN_HEIGHT // 4)
-
-        self.blue_tank = Tank('blue', SCREEN_WIDTH // 2, SCREEN_HEIGHT * 3 // 4)
-
         self.rockets = arcade.SpriteList()
-
-        self.game_over = False
+        self.tanks_list = arcade.SpriteList()
+        self.tanks_list.append(self.red_tank)
+        self.tanks_list.append(self.blue_tank)
+        self.keys_pressed = set()
 
     def on_draw(self):
-        arcade.start_render()
-
-        self.red_tank.draw()
-        self.blue_tank.draw()
+        self.clear()
+        arcade.draw_texture_rect(self.texture_back,
+                                 arcade.rect.XYWH(arcade.get_window().width // 2, arcade.get_window().height // 2,
+                                                  arcade.get_window().width, arcade.get_window().height))
+        self.tanks_list.draw()
         self.rockets.draw()
 
-        if self.game_over:
-            arcade.draw_text("GAME OVER!",
-                             SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
+        if self.game_over and self.winner == 'red':
+            arcade.draw_text("WINNER IS RED!",
+                             arcade.get_window().width // 2, arcade.get_window().height // 2,
                              arcade.color.RED, 50,
+                             anchor_x="center", anchor_y="center")
+
+        elif self.game_over and self.winner == 'blue':
+            arcade.draw_text("WINNER IS BLUE!",
+                             arcade.get_window().width // 2, arcade.get_window().height // 2,
+                             arcade.color.BLUE, 50,
                              anchor_x="center", anchor_y="center")
 
     def on_update(self, delta_time):
         if self.game_over:
             return
 
-        self.red_tank.update()
-        self.blue_tank.update()
+        self.red_tank.update(delta_time)
+        self.blue_tank.update(delta_time)
         self.rockets.update()
 
         for rocket in self.rockets:
             if rocket.owner_color == "red" and arcade.check_for_collision(rocket, self.blue_tank):
                 self.game_over = True
-                arcade.close_window()
+                self.winner = 'red'
             elif rocket.owner_color == "blue" and arcade.check_for_collision(rocket, self.red_tank):
                 self.game_over = True
-                arcade.close_window()
+                self.winner = 'blue'
 
     def on_key_press(self, key, modifiers):
+        self.keys_pressed.add(key)
         if self.game_over:
             return
-
-        if key == arcade.key.UP:
-            self.red_tank.change_y = TANK_SPEED
-            rocket = self.red_tank.shoot()
-            self.rockets.append(rocket)
-        elif key == arcade.key.DOWN:
-            self.red_tank.change_y = -TANK_SPEED
-            rocket = self.red_tank.shoot()
-            self.rockets.append(rocket)
-        elif key == arcade.key.LEFT:
-            self.red_tank.change_x = -TANK_SPEED
-            rocket = self.red_tank.shoot()
-            self.rockets.append(rocket)
-        elif key == arcade.key.RIGHT:
-            self.red_tank.change_x = TANK_SPEED
-            rocket = self.red_tank.shoot()
-            self.rockets.append(rocket)
-
-        if key == arcade.key.W:
+        if arcade.key.UP in self.keys_pressed:
             self.blue_tank.change_y = TANK_SPEED
-            rocket = self.blue_tank.shoot()
+            rocket = self.blue_tank.shoot(180)
             self.rockets.append(rocket)
-        elif key == arcade.key.S:
+        if arcade.key.DOWN in self.keys_pressed:
             self.blue_tank.change_y = -TANK_SPEED
-            rocket = self.blue_tank.shoot()
+            rocket = self.blue_tank.shoot(0)
             self.rockets.append(rocket)
-        elif key == arcade.key.A:
+        if arcade.key.LEFT in self.keys_pressed:
             self.blue_tank.change_x = -TANK_SPEED
-            rocket = self.blue_tank.shoot()
+            rocket = self.blue_tank.shoot(270)
             self.rockets.append(rocket)
-        elif key == arcade.key.D:
+        if arcade.key.RIGHT in self.keys_pressed:
             self.blue_tank.change_x = TANK_SPEED
-            rocket = self.blue_tank.shoot()
+            rocket = self.blue_tank.shoot(90)
+            self.rockets.append(rocket)
+
+        if arcade.key.W in self.keys_pressed:
+            self.red_tank.change_y = TANK_SPEED
+            rocket = self.red_tank.shoot(180)
+            self.rockets.append(rocket)
+        if arcade.key.S in self.keys_pressed:
+            self.red_tank.change_y = -TANK_SPEED
+            rocket = self.red_tank.shoot(0)
+            self.rockets.append(rocket)
+        if arcade.key.A in self.keys_pressed:
+            self.red_tank.change_x = -TANK_SPEED
+            rocket = self.red_tank.shoot(270)
+            self.rockets.append(rocket)
+        if arcade.key.D in self.keys_pressed:
+            self.red_tank.change_x = TANK_SPEED
+            rocket = self.red_tank.shoot(90)
             self.rockets.append(rocket)
 
     def on_key_release(self, key, modifiers):
+        if key in self.keys_pressed:
+            self.keys_pressed.remove(key)
+
         if key in [arcade.key.UP, arcade.key.DOWN]:
-            self.red_tank.change_y = 0
-        elif key in [arcade.key.LEFT, arcade.key.RIGHT]:
-            self.red_tank.change_x = 0
+            self.blue_tank.change_x = 0
+        if key in [arcade.key.LEFT, arcade.key.RIGHT]:
+            self.blue_tank.change_y = 0
 
         if key in [arcade.key.W, arcade.key.S]:
-            self.blue_tank.change_y = 0
-        elif key in [arcade.key.A, arcade.key.D]:
-            self.blue_tank.change_x = 0
+            self.red_tank.change_x = 0
+        if key in [arcade.key.A, arcade.key.D]:
+            self.red_tank.change_y = 0
 
 
 def main():
-    game = TankGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    game.setup()
+    game = TankGame()
     arcade.run()
 
 
 if __name__ == "__main__":
     main()
+
